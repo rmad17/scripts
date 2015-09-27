2af51f23ff8f983f68068a853e06d9ce1baa3af1
rmad17 <souravbasu17@gmail.com>
2015-09-27 14:38:24 +0530

    Added instructions

--- palm-detect.sh
+++ palm-detect.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
+# Run these commands:
+# chmod +x palm-detect.sh
+# sudo sed -i '/^exit 0/i /home/rmad/repos/scripts/palm-detect.sh &&' /etc/rc.local
 sleep 20
 synclient PalmDetect=1 && synclient PalmMinWidth=5
